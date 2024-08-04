5a4f67e7aa0d61df8ce003d1311a60a583f6e4f0
odhi99 <odhii9671@gmail.com>
2024-08-05 00:39:09 +0800

    fortify login, logout

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('pages.dashboard');
-// });
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
+});
 
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => '']);
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
 });
